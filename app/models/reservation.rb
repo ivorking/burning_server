8b1c1da6493982a0d1ccde1dcd5cc5f11e67d41c
Ivor King <ivorking@gmail.com>
2018-04-18 10:21:20 +1000

    fixed database

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
-    belongs_to :flight
-    belongs_to :user
+    belongs_to :flight, :optional => true
+    belongs_to :user, :optional => true
+    has_one :row, through: :flights
+    has_one :column, through: :flights
 end
