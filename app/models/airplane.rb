dc6310174c1205b70d156f15c8659cbe25a51e5a
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-04-18 10:41:14 +1000

    Annotate

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: airplanes
+#
+#  id         :integer          not null, primary key
+#  ship       :string
+#  row        :integer
+#  column     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Airplane < ApplicationRecord
     has_many :flights
 end
